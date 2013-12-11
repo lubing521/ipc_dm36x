c6445cbfffeffad0810ca01ed451bad33d87b2ad
jiangjinxiong <348248135@qq.com>
2013-12-11 22:52:43 +0800

    调整CMEM起始地址

--- av_capture/bin/av_capture_load.sh
+++ av_capture/bin/av_capture_load.sh
@@ -5,8 +5,7 @@
 ./csl_load.sh
 ./drv_load.sh
 
-#insmod cmemk.ko phys_start=0x83000000 phys_end=0x88000000 allowOverlap=1 phys_start_1=0x00001000 phys_end_1=0x00008000 pools_1=1x28672
-insmod cmemk.ko phys_start=0x83C00000 phys_end=0x88000000 allowOverlap=1 phys_start_1=0x00001000 phys_end_1=0x00008000 pools_1=1x28672
+insmod cmemk.ko phys_start=0x83800000 phys_end=0x88000000 allowOverlap=1 phys_start_1=0x00001000 phys_end_1=0x00008000 pools_1=1x28672
 
 insmod edmak.ko
 insmod irqk.ko
