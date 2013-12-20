916776da1fc5bf2acfaea4622e45d1158c1a4060
jiangjinxiong <348248135@qq.com>
2013-12-20 14:37:08 +0800

    完善sensorcheck

--- av_capture/application/test/sensorcheck/main.c
+++ av_capture/application/test/sensorcheck/main.c
@@ -3,20 +3,52 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef enum enumSensorType
+{
+    AR0130    = 0,
+    AR0331,
+    MT9M034,
+    MT9P031,
+    AP0101,
+    IMX122,
+    AR0330,
+    OV9712,
+    SENSOR_UNKNOWN,
+    SENSOR_NONE
+} eSensorType;
+
 int main(int argc, char **argv)
 {
-    int status = CheckSensor();
-    int sensorid = GetSensorId();
-    if (status != 0)
+    CheckSensor();
+    eSensorType sensorId = GetSensorId();
+    switch (sensorId)
     {
-        printf("Can not identify sensor!!!\n");
-        return -1;
-    }
-    else
-    {
-        printf("sensorId = %d\n", sensorid);
+    case OV9712:
+        printf("Sensor is OV9712\n");
+        break;
+    case AR0130:
+        printf("Sensor is AR0130\n");
+        break;
+    case MT9M034:
+        printf("Sensor is MT9M034\n");
+        break;
+    case IMX122:
+        printf("Sensor is IMX122\n");
+        break;
+    case AR0330:
+        printf("Sensor is AR0330\n");
+        break;
+    case AR0331:
+        printf("Sensor is AR0331\n");
+        break;
+    case MT9P031:
+        printf("Sensor is MT9P031\n");
+        break;
+    default:
+        printf("Sensor is error\n");
+        break;
     }
 
-    return sensorid;
+    return sensorId;
 }
 
